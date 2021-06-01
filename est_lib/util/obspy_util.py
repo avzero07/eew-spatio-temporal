5bb572e5b1930ec8298207b59ae31dc3fd42d548
avzero07 <akshay.viswakumar@gmail.com>
2021-06-01 16:13:14 -0700

    npy save/load util

--- est_lib/util/obspy_util.py
+++ est_lib/util/obspy_util.py
@@ -2,6 +2,7 @@
 Contains utility functions to interact with obspy.
 '''
 
+import numpy as np
 import os
 import obspy
 from obspy.clients.fdsn import Client
@@ -117,6 +118,40 @@ def stream_writer(stream_obj,file_path,file_format="MSEED"):
     f_path = os.path.abspath(file_path)
     stream_obj.write(f_path,format=file_format)
     return f_path
+
+def stream_data_writer(stream_obj,inv_obj,filepath,sta_list,
+                       chan_list):
+    '''
+    Writes stream data into a csv file in the files system.
+
+    Data stored as sequence x station x channel
+    '''
+    sta_list.sort()
+    chan_list.sort()
+
+    seq_length = len(stream_obj[0].data)
+    node_length = len(sta_list)
+    channel_length = len(chan_list)
+
+    filler = np.zeros([seq_length,node_length,channel_length])
+
+    for i,sta in enumerate(sta_list):
+        for j,chan in enumerate(chan_list):
+            temp = stream_obj.select(station=sta,
+                                     channel=chan)
+            filler[:,i,j] = temp[0].data
+
+    with open(filepath,'wb') as f:
+        np.save(f,filler)
+    return filepath
+
+def stream_data_reader(filepath):
+    '''
+    Reads data stored by stream_data_writer and returns
+    a numpy array.
+    '''
+    with open(filepath,'rb') as f:
+        return np.load(f,allow_pickle=True)
 
 # Methods to Read Data from Filesystem
 
